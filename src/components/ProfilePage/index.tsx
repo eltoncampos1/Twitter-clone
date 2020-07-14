118cf073320fadabc3b641c430e91b8be9f08dbd
eltoncampos1 <qazwsx272518>
2020-07-14 19:53:17 -0300

    Criação dos components

--- src/components/ProfilePage/index.tsx
+++ src/components/ProfilePage/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Feed from '../Feed';
 
 import {
    Container, 
--- src/styles/icons.ts
+++ src/styles/icons.ts
@@ -7,6 +7,6 @@ export {
   LocationOn,
   Search,
 }from 'styled-icons/material-outlined';
-export {  Person, Cake, Favorite} from 'styled-icons/material';
+export {  Person, Cake, Favorite, } from 'styled-icons/material';
 export { ArrowLeft } from 'styled-icons/heroicons-solid';
-export { Chat } from 'styled-icons/heroicons-outline';
+export { Chat, SwitchHorizontal } from 'styled-icons/heroicons-outline';
